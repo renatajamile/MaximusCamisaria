0d514ef2f9df4504fb4d66c5b49bdc48487c02e0
renatajamile <renatajamiler@gmail.com>
2024-04-20 17:03:47 -0300

    .

--- MaximusCamisaria/routes.py
+++
@@ -1,7 +0,0 @@
-
-from flask import render_template, url_for
-from MaximusCamisaria import app
-
-@app.route("/")
-def homepage():
-    return render_template("home.html")
--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from MaximusCamisaria import app
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+    return render_template("home.html")
+
+@app.route("/perfil/<usuario>")
+def perfil(usuario):
+    return render_template("perfil.html", usuario=usuario)
 
 if __name__ == "__main__":
     app.run(debug=True)
