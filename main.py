a94dc9140769e57a5abcce07dbe9c2dd62684fb9
renatajamile <renatajamiler@gmail.com>
2024-04-20 17:58:21 -0300

    2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='MaximusCamisaria/templates/')
 
 @app.route("/")
 def homepage():
@@ -9,6 +9,14 @@ def homepage():
 @app.route("/perfil/<usuario>")
 def perfil(usuario):
     return render_template("perfil.html", usuario=usuario)
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/produtos")
+def produtos():
+    return render_template("produtos.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
