3d1e248df1e2f764eac55a69a1cf1991494e381c
renatajamile <renatajamiler@gmail.com>
2024-04-22 20:18:21 -0300

    alterações 22-04

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__, template_folder='MaximusCamisaria/templates/')
+app = Flask(__name__, template_folder='templates/')
 
 @app.route("/")
 def homepage():
