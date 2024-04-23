ab8048005dafaae498c287bdc88f4c1e4234f06f
renatajamile <renatajamiler@gmail.com>
2024-04-22 22:17:43 -0300

    A6

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='templates/')
 
 @app.route("/")
 def homepage():
-    return render_template("home.html")
+    return render_template("estrutura.html")
 
 @app.route("/perfil/<usuario>")
 def perfil(usuario):
