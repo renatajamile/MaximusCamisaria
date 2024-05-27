ffc05153306c9db47bc7b05ab15422741bdf62a0
marcellerabelo <107086283+marcellerabelo@users.noreply.github.com>
2024-05-26 22:43:32 -0300

    Atualização da pagina.

--- loja/forms.py
+++ loja/forms.py
@@ -1,13 +1,13 @@
 # criar os formulários do site
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import PasswordField, StringField, SelectField, IntegerField, HiddenField, TextAreaField, SubmitField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
-from loja.models import Usuario 
+from loja.models import Usuario, Personalizada
 
 class FormLogin(FlaskForm):
     email = StringField("E-mail", validators=[DataRequired(), Email()])
     senha = PasswordField("Senha", validators=[DataRequired()])
-    botao_confirmacao = SubmitField("Entrar")
+    botao_confirmacao = SubmitField("ENTRAR")
 
 class FormCriarConta(FlaskForm):
     username = StringField ("Nome do usuário", validators=[DataRequired()])
@@ -22,7 +22,7 @@ class FormCriarConta(FlaskForm):
     uf = StringField("UF", validators=[DataRequired()])
     cep = StringField("CEP", validators=[DataRequired()])
     telefone = StringField("Telefone", validators=[DataRequired()])
-    botao_confirmacao = SubmitField("Cadastrar")
+    botao_confirmacao = SubmitField("CADASTRAR")
 
     def validate_email(self, email):
         usuario = Usuario.query.filter_by(email=email.data).first()
@@ -33,3 +33,22 @@ class FormCriarConta(FlaskForm):
         usuario = Usuario.query.filter_by(cpf=cpf.data).first()
         if usuario:
             return ValidationError("CPF já cadastrado, faça login para continuar")
+
+class FormPersonalizada(FlaskForm):
+    foto = FileField('Foto', validators=[DataRequired()])
+    categorias = [('personalizada', 'Personalizada'), ('tradicional', 'Tradicional'), ('raglan', 'Raglan'),
+        ('esportiva', 'Esportiva'), ('uniforme', 'Escolar'), ('longa', 'Manga Longa'), 
+        ('golaV', 'Gola V'), ('golapolo', 'Gola Polo'), ('oversized', 'Oversized')]
+    tamanhos = [('pp', 'PP'), ('p', 'P'), ('m', 'M'), ('g', 'G'), ('gg', 'GG')]
+    tecidos = [('algodao', 'Algodão'), ('piquet', 'Piquet'), ('dryfit', 'Dry Fit'), 
+        ('pvantipiling', 'PV Antipiling')]
+    cores = [('#ff0000', 'Vermelho'), ('#0000ff', 'Azul'), ('#00ff00', 'Verde'), 
+        ('#000000', 'Preto'), ('#ffffff', 'Branco')]
+    categoria = SelectField('Categoria', choices=categorias, validators=[DataRequired()])
+    tamanho = SelectField('Tamanho', choices=tamanhos, validators=[DataRequired()])
+    quantidade = IntegerField('Quantidade', validators=[DataRequired()])
+    tecido = SelectField('Tecido', choices=tecidos, validators=[DataRequired()])
+    cor = HiddenField('Cor', validators=[DataRequired()])
+    texto_camisa = StringField('Texto na Camisa', validators=[DataRequired()])
+    observacao = TextAreaField('Observações')
+    botao_confirmacao = SubmitField('SOLICITAR ORÇAMENTO')
